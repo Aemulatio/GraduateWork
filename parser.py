9715a6352babb9a7c5c2ee193cd3658996d8c2a2
Stepan S <dumn2011@gmail.com>
2019-10-10 00:34:49 +0900

    renamed method, almost done it

--- parser.py
+++ parser.py
@@ -7,26 +7,44 @@ def get_html(url):
     r = requests.get(url)
     return r.text if r.status_code == 200 else "Error"
 
+
 def get_DOM(url):
     html = get_html(url)
     soup = BeautifulSoup(html, 'lxml')
     # return soup.find('table', class_='stats-table')
     return soup.find_all('tr')
 
+
 def get_all_elements(url):
     tbl = get_DOM(url)
     tbl.pop(0)
-    elements = []    
+    elements = []
     for tr in tbl:
         elements.append(tr)
     return elements
 
-def get_data(url):  #rename later
-     # тут выделение каждого элемента из массива элементс
-     #
-     #
-     #
-     #
+
+def get_data(url):  # rename later
+    elements = get_all_elements(url)
+    data = []
+    for element in elements:
+        teams = []
+        score = []
+        date = element.find("div", class_="time").text.replace('/', '-')
+        teams_ = element.find_all("td", class_="team-col")        
+        for t in teams_:
+            teams.append(t.find("a").text)
+            score.append(t.find("span", class_ = "score").text.strip().replace(')', '').replace('(', '')) 
+        # score = element.find_all("td", class_="score")
+        playedMap = element.find("div", class_="dynamic-map-name-full").text
+        event = element.find("td", class_="event-col").text
+        data.append((date, str(teams), str(score), playedMap, event))
+    return data
+    # тут выделение каждого элемента из массива элементс
+    # сделать компановку по матчам
+    #
+    #
+    #
 
 # def create_csv(url):
     ###
@@ -35,7 +53,7 @@ def get_data(url):  #rename later
 
 url = "https://www.hltv.org/stats/matches?startDate=2018-10-09&endDate=2019-10-09&offset=100"
 
-print(get_all_elements(url))
+print(get_data(url))
 
 
 # html_doc = urlopen('http://otus.ru').read()
