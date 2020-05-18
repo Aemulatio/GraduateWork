90802447179bd3f82c0b1b85c39ae70839c62787
Stepan S <dumn2011@gmail.com>
2020-05-18 23:52:52 +0900

    Add func to read CSV, and start trying to do train dict

---
+++ divideData.py
@@ -0,0 +1,39 @@
+import csv
+import collections
+
+
+def csv_reader(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        return [row for row in reader]
+
+
+if __name__ == "__main__":
+    data = csv_reader('refactoredData.csv')[1:]
+
+    # region test
+    wins = collections.defaultdict(int)
+    matches = collections.defaultdict(int)
+    maps = collections.defaultdict(int)
+    index = round(len(data) / 5)
+    print(index)
+    # ['KOVA', 'l', 'LDLC', 'w', '11-5-20 ', 'Dust2', 'Home Sweet Home Cup 5 Closed Qualifier']
+
+    for row in data[:index]:
+        # for t1 in [*row]:
+        #     print(t1)
+        matches[row[0].strip()] += 1
+        matches[row[2].strip()] += 1
+        maps[row[-2].strip()] += 1
+        if row[1].strip() == 'l':
+            wins[row[2].strip()] += 1
+        else:
+            wins[row[0].strip()] += 1
+
+    # print(row)
+    # print(data[0])
+    print(matches)
+    print(wins)
+    print(maps)
+
+    # endregion
