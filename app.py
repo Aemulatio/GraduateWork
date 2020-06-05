da6a7f83be8a14aa517198afad81d79c2e9772b5
Stepan S <dumn2011@gmail.com>
2020-06-05 18:50:13 +0900

    Надеюсь утечки больше не будет

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def predict(t1, t2, map):
     else:
         ret = t2
 
+    del vvod
+    del UniqueTeams
+    del UniqueMaps
+    del model
     return ret
 
 
@@ -47,31 +51,36 @@ from sklearn.model_selection import train_test_split
 from sklearn import ensemble
 
 
+def train_forest():
+    data = pd.read_csv("Data/results1_wo_garbage_NTN.csv")
+    UniqueTeams = pd.Series(np.unique(np.concatenate((data['Team1'].unique(), data['Team2'].unique()))))
+    # Получаем все карты, на которых играли команды
+    UniqueMaps = pd.Series(np.unique(data['Map'].unique()))
+    data = data.drop(['Team1_Score', 'Team2_Score'], 1)
+
+    X_all = data.drop(['Winner'], 1)
+    y_all = data['Winner']
+
+    for id, team in UniqueTeams.items():
+        X_all = X_all.replace(team, id)
+    for id, map in UniqueMaps.items():
+        X_all = X_all.replace(map, id)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_all, y_all,
+                                                        test_size=0.28,
+                                                        random_state=42, )
+
+    rf = ensemble.RandomForestClassifier(n_estimators=170, random_state=11)
+    rf.fit(X_train, y_train)
+    pickle.dump(rf, open("Models/RANDOM_FOREST.pickle", 'wb'))
+    del rf, X_test, X_train, y_train, y_test
+
+
 @app.route('/', methods=['post', 'get'])
 def main():
     data = pd.read_csv("Data/results1_wo_garbage_NTN.csv")
     if "RANDOM_FOREST.pickle" not in os.listdir("Models/"):
-        UniqueTeams = pd.Series(np.unique(np.concatenate((data['Team1'].unique(), data['Team2'].unique()))))
-        # Получаем все карты, на которых играли команды
-        UniqueMaps = pd.Series(np.unique(data['Map'].unique()))
-        data = data.drop(['Team1_Score', 'Team2_Score'], 1)
-
-        X_all = data.drop(['Winner'], 1)
-        y_all = data['Winner']
-
-        for id, team in UniqueTeams.items():
-            X_all = X_all.replace(team, id)
-        for id, map in UniqueMaps.items():
-            X_all = X_all.replace(map, id)
-
-        X_train, X_test, y_train, y_test = train_test_split(X_all, y_all,
-                                                            test_size=0.28,
-                                                            random_state=42, )
-
-        rf = ensemble.RandomForestClassifier(n_estimators=170, random_state=11)
-        rf.fit(X_train, y_train)
-        pickle.dump(rf, open("Models/RANDOM_FOREST.pickle", 'wb'))
-
+        train_forest()
     if request.method == 'POST':
         # print(str(request.form.get('team1')))
         t1 = request.form.get('team1')
