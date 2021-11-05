869fe8b3529fda8b6d1d2a31b4b3f1e420fe8ddc
Stiiipan <dumn2011@gmail.com>
2021-11-05 17:03:44 +0900

    Запись в БД Mongo

---
+++ pythonScripts/writeToDB.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import json
+
+
+def setTeams(input_file: str):
+    client = MongoClient(
+        "mongodb+srv://new:oIGh34Xd8010lrgj@cluster0.rg6wi.mongodb.net/Cluster0?retryWrites=true&w=majority")
+    db = client.Diploma
+    collection = db.Teams
+    f = open(input_file, 'r', encoding='utf-8')
+    data = json.loads(f.read())
+    for row in data.items():
+        collection.insert_one({row[0]: row[1]})
+
+
+if __name__ == '__main__':
+    setTeams("../Data/New/teams.json")
