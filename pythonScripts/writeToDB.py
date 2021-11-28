0e263ff7093d23991ac2ae005d70ba82b949d369
Stiiipan <dumn2011@gmail.com>
2021-11-28 13:16:31 +0900

    Данные удаляются и по новой пишутся теперь и карты

--- pythonScripts/writeToDB.py
+++ pythonScripts/writeToDB.py
@@ -7,6 +7,8 @@ def setTeams(input_file: str):
         "mongodb+srv://new:oIGh34Xd8010lrgj@cluster0.rg6wi.mongodb.net/Cluster0?retryWrites=true&w=majority")
     db = client.Diploma
     collection = db.Teams
+    for obj in collection.find():
+        collection.delete_many(obj)
     f = open(input_file, 'r', encoding='utf-8')
     data = json.loads(f.read())
     for row in data.items():
@@ -14,5 +16,20 @@ def setTeams(input_file: str):
                                row[0]: row[1]})
 
 
+def setMaps(input_file: str):
+    client = MongoClient(
+        "mongodb+srv://new:oIGh34Xd8010lrgj@cluster0.rg6wi.mongodb.net/Cluster0?retryWrites=true&w=majority")
+    db = client.Diploma
+    collection = db.Maps
+    for obj in collection.find():
+        collection.delete_many(obj)
+    f = open(input_file, 'r', encoding='utf-8')
+    data = json.loads(f.read())
+    for row in data.items():
+        collection.insert_one(row[1])
+        # print(row[1])
+
+
 if __name__ == '__main__':
     setTeams("../Data/New/teams.json")
+    setMaps("../Data/New/maps.json")
